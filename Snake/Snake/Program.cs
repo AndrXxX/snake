fb3f06d9a0be46b5159a9caf011fff977a439c40
AndrXxX <andrew1gar@yandex.ru>
2016-02-02 21:56:55 +0300

    Изучение класса List

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -16,6 +16,34 @@ namespace Snake
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
+            Point p3 = new Point(6, 7, '$');
+            p3.Draw();
+
+            Point p4 = new Point(8, 9, '&');
+            p4.Draw();
+
+            List<char> numList = new List<char>();
+            numList.Add('a');
+            numList.Add('b');
+            numList.Add('c');
+
+            char x = numList[0];
+            char y = numList[1];
+            char z = numList[2];
+
+            foreach (int i in numList)
+            {
+                Console.WriteLine(i);
+            }
+
+            numList.RemoveAt(0);
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+            pList.Add(p3);
+            pList.Add(p4);
+
 
             Console.ReadLine();
         }
